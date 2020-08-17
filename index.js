df5056b0cb2ebca34ad1350385611a233c39e10e
qtcoder999 <30284500+qtcoder999@users.noreply.github.com>
2020-08-17 23:58:00 +0530

    Create index.js

---
+++ index.js
@@ -0,0 +1,64 @@
+(function() {
+    console.clear();
+    let intervalId;
+    let timeGap = 3000;
+    
+    keepClickingSeeMoreRequestsButton();
+    //     startCancellingRequests();
+
+    function keepClickingSeeMoreRequestsButton() {
+        let seeMoreRequestsButtonSelector = ".pam.uiBoxLightblue._5cz.uiMorePagerPrimary";
+        intervalId = setInterval(()=>{
+            try {
+                document.querySelector(seeMoreRequestsButtonSelector).click();
+            } catch (e) {
+                console.log(e);
+                clearInterval(intervalId);
+                startCancellingRequests();
+            }
+        }
+        , 2000);
+    }
+
+    function startCancellingRequests() {
+        var selector = "button._42ft._4jy0.FriendRequestOutgoing.enableFriendListFlyout.outgoingButton.enableFriendListFlyout._4jy3._517h._51sy";
+        var cancelSelector = ".FriendListActionMenu .FriendListCancel";
+
+        var divs = document.querySelectorAll(selector);
+
+        //         for (let i = 0; i < 5; i++) {
+        for (let i = 0; i < divs.length; i++) {
+
+            setTimeout(()=>{
+                console.log("cancel click executed")
+                document.querySelector(cancelSelector).click();
+            }
+            , i * timeGap + 500);
+
+            setTimeout(function timer() {
+                let currentDiv = divs[i];
+
+                console.log(currentDiv.innerText, "found");
+                simulateMouseover(currentDiv);
+
+            }, i * timeGap);
+
+        }
+    }
+
+    function simulateMouseover(myTarget) {
+        var event = new MouseEvent('mouseover',{
+            'view': window,
+            'bubbles': true,
+            'cancelable': true
+        });
+        var canceled = !myTarget.dispatchEvent(event);
+        if (canceled) {// A handler called preventDefault.
+        //         alert("canceled");
+        } else {// None of the handlers called preventDefault.
+        //         alert("not canceled");
+        }
+    }
+
+}
+)()
